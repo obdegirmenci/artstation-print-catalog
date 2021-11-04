98dc7027684a564ab9166532b602feedce513fb7
obdegirmenci <22788481+obdegirmenci@users.noreply.github.com>
2021-11-04 23:26:27 +0300

    UI

--- js/func.js
+++ js/func.js
@@ -1,3 +1,38 @@
 function a(x) {console.log(x)};
 
-document.addEventListener('DOMContentLoaded', a("Hello wo... opps"), false);
+document.addEventListener('DOMContentLoaded', pageReady(), false);
+
+function pageReady() {
+    const form = document.getElementById("catalog");
+    const submitButton = document.getElementById("submit");
+
+    form.addEventListener('click', e => {
+        if (e.target.id == "submit") {
+            e.preventDefault();
+            let data = new FormData(form);
+            
+            for (let [key, value] of data) {
+                a(`${key}: ${value}`);
+                /*
+                if (key === "length_unit") {
+                    a(`${value}`);
+                    //a(`${key}: ${value}`);
+                }
+                */
+                /*
+                switch (key) {
+                    case "length_unit":
+                        a(`${value}`);
+                        break;
+                
+                    default:
+                        break;
+                }
+                */
+            }
+
+            //a(data.getAll("product_types"))
+            
+        }
+    });
+}
