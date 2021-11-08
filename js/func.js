e385a9ac065f1000d960309cff2f6521742f1319
obdegirmenci <22788481+obdegirmenci@users.noreply.github.com>
2021-11-08 05:01:55 +0300

    Comments & Convert to Functions

--- js/func.js
+++ js/func.js
@@ -1,3 +1,7 @@
+function log(x) { console.log(x) };
+
+// All available print sizes that in landscape orientation.
+// Portraits are calculated with on the fly just by reversing them.
 const printSizes = {
   "1:1" : ["8x8", "10x10", "12x12", "16x16", "20x20", "24x24", "30x30", "36x36"],
   "5:4" : ["10x8", "20x16", "30x24"],
@@ -6,20 +10,119 @@ const printSizes = {
   "2:1" : ["20x10"]
 };
 
-function a(x) {console.log(x)};
+/**
+ * Returns input list of form which is sortable.
+ *
+ * @param {object} form The loaded form from page.
+ * @return {array} Sortable item list.
+ */
+function getSortableList(form) {
+    let items = [];
+            
+    for (let key of form.keys()) {
+        if (key !== "product_types") {
+            items.push(key);
+            //log(`${key} is Sortable`);
+        }
+    }
+
+    return items;
+}
+
+/**
+ * Creates sortable item list.
+ *
+ * @param {object} el The element that new items will be created inside.
+ * @param {array} items The form items that will be used as sortable list items.
+ */
+function createSortItems(el, items) {
+    
+}
+
+/**
+ * Calculates digital resolution of prints by given size in inch.
+ *
+ * @param {string} dimension The width and height of print.
+ * @return {string} Minimum resolution that should be at 300PPI.
+ */
+function calcResolution(dimension) {
+    let ppi = 300;
+    let resolutionX = dimension.split("x")[0] * ppi;
+    let resolutionY = dimension.split("x")[1] * ppi;
+    let resolution = `${resolutionX}x${resolutionY}`;
+    return resolution;
+}
+
+/**
+ * Updates the SELECT options based on user selections in the form.
+ *
+ * @param {object} el Which SELECT element will be updated. Values are "size" or "aspect_ratio".
+ * @param {string} orientation The orientation value of SELECT.
+ * @param {string} aspect_ratio The aspect ratio value of SELECT.
+ */
+function updateSelect(el, orientation, aspect_ratio) {
+    let fragment = new DocumentFragment();
+
+      if (el.id === "size") {
+        for(let size of printSizes[aspect_ratio]) {
+
+          if (orientation === "portrait") {
+            size = size.split("x").reverse().join("x");
+          }
+
+          let option = document.createElement("option");
+          option.value = size;
+          option.innerHTML = `${size}: ${calcResolution(size)}`;
+          fragment.appendChild(option);
+        }
+      } else if (el.id === "aspect_ratio") {
+        for(let ratio of Object.keys(printSizes)) {
+          let option = document.createElement("option");
+
+          if (orientation === "square" && ratio === "1:1") {
+            // If is square
+            log(ratio);
+            option.value = ratio;
+            option.innerHTML = `${ratio}`;
+            fragment.appendChild(option);
+          } else if (orientation === "landscape" && ratio !== "1:1") {
+            // If is landscape
+            log(ratio);
+            option.value = ratio;
+            option.innerHTML = `${ratio}`;
+            fragment.appendChild(option);
+          } else if (orientation === "portrait" && ratio !== "1:1") {
+            // If is portrait
+            option.value = ratio;
+            ratio = ratio.split(":").reverse().join(":");
+            log(ratio);
+            option.innerHTML = `${ratio}`;
+            fragment.appendChild(option);
+          }
+          
+        }
+        
+      }
+
+    el.replaceChildren(fragment);
+}
 
 document.addEventListener('DOMContentLoaded', pageReady(), false);
 
 function pageReady() {
     const form = document.getElementById("catalog");
     const submitButton = document.getElementById("submit");
+
     const userOrientation = document.getElementById("orientation");
     const userAspectRatio = document.getElementById("aspect_ratio");
-    const sizeEl= document.getElementById("size");
+    const userSize= document.getElementById("size");
     
-    updateSelect(userAspectRatio, userOrientation.value, undefined);
-    //updateSelect(sizeEl, userOrientation.value, userAspectRatio.value);
-    //let uid = `${uid1}-${uid2}-${uid3}`;
+    // Initiale the SELECTs.
+    updateSelect(userAspectRatio, userOrientation.value, userAspectRatio);
+    updateSelect(userSize, userOrientation.value, userAspectRatio.value);
+
+    let data = new FormData(form);
+    getSortableList(data);
 
     form.addEventListener('change', e => {
 
@@ -52,11 +155,11 @@ function pageReady() {
                     */
                 }
                 updateSelect(userAspectRatio, userOrientation.value, userAspectRatio.value);
-                updateSelect(sizeEl, userOrientation.value, userAspectRatio.value);
+                updateSelect(userSize, userOrientation.value, userAspectRatio.value);
                 break;
             case "aspect_ratio":
-                a(`${e.target.value} is selected`);
-                updateSelect(sizeEl, userOrientation.value, userAspectRatio.value);
+                log(`${e.target.value} is selected`);
+                updateSelect(userSize, userOrientation.value, userAspectRatio.value);
                 break;
                 
             default:
@@ -66,28 +169,26 @@ function pageReady() {
 
     form.addEventListener('click', e => {
         if (e.target.id == "submit") {
+            // Don't submit to anywhere
             e.preventDefault();
-            let data = new FormData(form);
+            // Update the data with current form values
+            data = new FormData(form);
 
             let uid1;
             let userId = data.get("id");
             let uid2 = "0101";
 
-            let productWidth = data.get("width");
-            let productHeight = data.get("height");
-
-            //a(calcDimension(productWidth,productHeight));
-
+            // Produce as much as given Product Type
             for (let print of data.getAll("product_types")) {
-                //a(print);
+                //log(print);
                 
                 for (let [key, value] of data) {
-                    //a(`${key}: ${value}`);
+                    //log(`${key}: ${value}`);
                     /*
                     switch (key) {
                         case "title":
                             
-                            a(`${key}: ${value}`);
+                            log(`${key}: ${value}`);
                             break;
                     
                         default:
@@ -97,8 +198,8 @@ function pageReady() {
                     
                 }
 
+                // Don't include Product Type.
                 data.delete("product_types");
-                //data.set("product_types", print);
                 
                 switch (print) {
                     case "art_poster":
@@ -122,11 +223,12 @@ function pageReady() {
                 data.set("id", newId);
 
                 // Correct the ratio if is portrait just before creating result.
+                // Works multiple times and gives error. It needs to work just one time.
                 if (userOrientation.value === "portrait") {
                   let ratio = data.get("aspect_ratio");
                   correctedRatio = ratio.split(":").reverse().join(":");
                   data.set("aspect_ratio", correctedRatio);
-                  a(`${data.get("aspect_ratio")} (original ${ratio})`);
+                  log(`${data.get("aspect_ratio")} (original ${ratio})`);
                 }
 
                 let result = [];
@@ -134,7 +236,7 @@ function pageReady() {
                     
                     result.push(value);
                 }
-                a(result.join(", "));
+                log(result.join(", "));
                 
             }
 
@@ -142,74 +244,4 @@ function pageReady() {
             
         }
     });
-}
-
-function calcDimension(width, height) {
-    let productRatio = width / height;
-
-    switch (productRatio) {
-        case 1:
-            a(productRatio + " Landscape");
-            break;
-        case 2:
-            a(productRatio + " Landscape");
-            break;
-    
-        default:
-            break;
-    }
-}
-
-function updateSelect(el, orientation, aspect_ratio) {
-    let fragment = new DocumentFragment();
-
-      if (el.id === "size") {
-        for(let size of printSizes[aspect_ratio]) {
-
-          if (orientation === "portrait") {
-            size = size.split("x").reverse().join("x");
-          }
-  
-          let ppi = 300;
-          let resolutionX = size.split("x")[0] * ppi;
-          let resolutionY = size.split("x")[1] * ppi;
-          let resolution = `${resolutionX}x${resolutionY}`;
-          
-          //a(`${orientation} ${size}: ${resolution}`);
-  
-          let option = document.createElement("option");
-          option.value = size;
-          option.innerHTML = `${size}: ${resolution}`;
-          fragment.appendChild(option);
-        }
-      } else if (el.id === "aspect_ratio") {
-        for(let ratio of Object.keys(printSizes)) {
-          let option = document.createElement("option");
-
-          if (orientation === "square" && ratio === "1:1") {
-            // If is square
-            a(ratio);
-            option.value = ratio;
-            option.innerHTML = `${ratio}`;
-            fragment.appendChild(option);
-          } else if (orientation === "landscape" && ratio !== "1:1") {
-            // If is landscape
-            a(ratio);
-            option.value = ratio;
-            option.innerHTML = `${ratio}`;
-            fragment.appendChild(option);
-          } else if (orientation === "portrait" && ratio !== "1:1") {
-            // If is portrait
-            option.value = ratio;
-            ratio = ratio.split(":").reverse().join(":");
-            a(ratio);
-            option.innerHTML = `${ratio}`;
-            fragment.appendChild(option);
-          }
-          
-        }
-        
-      }
-
-    el.replaceChildren(fragment);
 }
